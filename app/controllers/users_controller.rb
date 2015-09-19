aede127ad0d0ec4335f26a99bd08b92a392ea762
Mitchell Gu <me@mitchgu.com>
2015-09-19 14:33:41 -0400

    Basic user signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
 
-    def signup
-    end
+  def signup
+    name = params[:name]
+    username = params[:username]
+    password = params[:password]
 
-    def login
+    user = User.new(name: name, username: username)
+    user.password = password
+
+    if user.save
+      render json: {:success => true}
+    else
+      render json: {:success => false, :errors => user.errors.full_messages}
     end
+  end
+
+  def login
+    username = params[:username]
+    password = params[:password]
+
+    user = User.find_by(username: username)
+    success = false
+    unless user.nil?
+      success = true if user.validate_password(password)
+    end
+    render json: {:success => success}
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,33 @@
+require 'password_helper'
+
 class User < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :username, presence: true, length: { maximum: 20 }, uniqueness: { case_sensitive: false }
+  validates :hashed_password, presence: true
+  validate :unhashed_password_cannot_be_empty
+
+  include PasswordHelper
+
+  def password=(password)
+    @password = password
+    if !password.nil?
+      self.hashed_password = createHash(password)
+    end
+  end
+
+  def password
+    @password || ""
+  end
+
+  def validate_password(password)
+    validatePassword(password, self.hashed_password)
+  end
+
+  def unhashed_password_cannot_be_empty
+    if password.nil?
+      errors.add(:password, "password cannot be empty")
+    end
+  end
+
 end
